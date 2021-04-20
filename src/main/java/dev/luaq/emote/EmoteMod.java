8ab76e57458d5951348b0421420391cab773bae4
luaqs <joshbirchlol@gmail.com>
2021-04-20 21:24:36 +0100

    add: mod instance & config

--- src/main/java/dev/luaq/emote/EmoteMod.java
+++ src/main/java/dev/luaq/emote/EmoteMod.java
@@ -1,13 +1,31 @@
 package dev.luaq.emote;
 
+import dev.luaq.emote.config.ConfigHandler;
+import dev.luaq.emote.config.EmoteConfig;
+import lombok.Getter;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "emote", version = "1.0")
 public class EmoteMod {
+    @Mod.Instance
+    public static EmoteMod instance;
+
+    @Getter private ConfigHandler<EmoteConfig> config;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        // create the config handler
+        config = new ConfigHandler<>(event.getSuggestedConfigurationFile(), new EmoteConfig());
+
+        // save it on shutdown
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> config.populate()));
+    }
+
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        // first commit, what you looking for?
+        
     }
 }
