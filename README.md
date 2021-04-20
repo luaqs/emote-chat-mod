1436fbc6183f047a2513969bb300d4f1f6ad083c
luaqs <joshbirchlol@gmail.com>
2021-04-20 02:34:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Emote Mod
+This mod allows for use of emojis in Minecraft chat.
---
+++ src/main/java/dev/luaq/emote/EmoteMod.java
@@ -0,0 +1,13 @@
+package dev.luaq.emote;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = "emote", version = "1.0")
+public class EmoteMod {
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        // first commit, what you looking for?
+    }
+}
